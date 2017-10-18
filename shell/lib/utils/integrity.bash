9f5e66563354652518b4298564b453e7beb394ff
build <build@fake.com>
2017-10-17 21:26:32 -0400

    remove another chef folder from integrity

--- shell/lib/utils/integrity.bash
+++ shell/lib/utils/integrity.bash
@@ -5,14 +5,14 @@ function get_source_latest(){
   local temp_ruby_bin=$(which ruby)
   fail_if 'not able to find any ruby installation, at least 1.9 is a prerequisite'
 	RUBYOPT='' $temp_ruby_bin -e "print File.mtime(Dir.glob('$1/**/*').max_by {|source_file| File.mtime(source_file)})"
-  fail_if 'not able to get latest source'
+  fail_if "not able to get latest source: $1"
 }
 
 function get_file_time(){
   local temp_ruby_bin=$(which ruby)
   fail_if 'not able to find any ruby installation, at least 1.9 is a prerequisite'
   RUBYOPT='' $temp_ruby_bin -e "print File.mtime('$1')"
-  fail_if 'not able to get file time'
+  fail_if "not able to get file time: $1"
 }
 
 function set_integrity() {
@@ -33,7 +33,6 @@ function assert_set_env_is_up_to_date(){
   export current_bash_source_latest="$(get_source_latest "${PEMBA_PATHS_SHELL_HOME}")"
   export current_bash_source_latest="$current_bash_source_latest$(get_source_latest "${PATHS_PROJECT_WORKSPACE_SETTINGS_SHELL_HOME}")"
   export current_bash_source_latest="$current_bash_source_latest$(get_file_time "${PATHS_PROJECT_HOME}/.pemba")"
-  export current_bash_source_latest="$current_bash_source_latest$(get_source_latest "${PATHS_PROJECT_WORKSPACE_SETTINGS_HOME}/chef")"
 
   if [[ "$current_bash_source_latest" != "$BASH_SOURCE_LOCK" ]]; then
     fail "
